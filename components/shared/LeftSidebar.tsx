f445ad74cc28e801d32e02d989babb43dfd57469
Sanjay Sirangi <sanjaysirangi@gmail.com>
2023-08-30 14:26:00 +0530

    added constants/index.js

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,8 +1,27 @@
+"use client";
 import React from 'react'
-
-function LeftSidebar()  {
+import { sidebarLinks } from "@/constants";
+import Link from 'next/link';
+import Image from 'next/image';
+import { usePathname , useRouter } from "next/navigation";
+function LeftSidebar() {
+  const router = useRouter();
+  const pathname = usePathname();
   return (
-    <div>LeftSidebar</div>
+    <section className='custom-scrollbar leftsidebar'>
+      <div className='flex w-full flex-1 flex-col gap-6 px-6'>
+        {sidebarLinks.map((link) => {
+          const isActive = (pathname.includes(link.route) && link.route.length > 1 ) || (pathname === link.route);
+          return (
+            <Link href={link.route} key={link.label} className={`leftsidebar_link ${isActive && `bg-primary-500` }`} >
+              <Image src={link.imgURL} alt={link.label} width={24} height={24} />
+              <p className='text-light-1 max-lg:hidden'>{link.label}</p>
+            </Link>
+          )
+        })}
+      </div>
+
+    </section>
   )
 }
 
--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,9 +1,37 @@
+import { OrganizationSwitcher, SignedIn, SignOutButton } from '@clerk/nextjs'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 function Topbar() {
-  return (
-    <div>Topbar</div>
-  )
+    return (
+        <nav className='topbar'>
+            <Link href="/" className='flex items-center gp-4'>
+                <Image src="/assets/logo.svg" alt="logo" width={28} height={28} />
+                <p className='text-heading3-bold text-light-1 max-xs:hidden'>Threads</p>
+
+            </Link>
+            <div className='flex items-center gap-1'>
+                <div className='block md:hidden'>
+                    <SignedIn>
+                        <SignOutButton>
+                            <div className='flex cursor-pointer'>
+                                <Image src="/assets/logout.svg" alt="logoout" width={24} height={24}  />
+                            </div>
+                        </SignOutButton>
+                    </SignedIn>
+                    { }
+                </div>
+                <OrganizationSwitcher  
+                appearance={{
+                    elements:{
+                        organizationSwitcherTrigger:"py-2 px-4"
+                    }
+                }}
+                />
+            </div>
+        </nav>
+    )
 }
 
 export default Topbar
