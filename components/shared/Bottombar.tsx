9b32fda898ba34edb92288239d9b8a35e33e4b82
Sanjay Sirangi <sanjaysirangi@gmail.com>
2023-08-30 15:22:16 +0530

    bottom bar finish

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -1,8 +1,31 @@
+"use client";
+import { sidebarLinks } from '@/constants'
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+// import { useRouter } from 'next/router';
 import React from 'react'
 
 function Bottombar() {
+  // const router = useRouter();
+  const pathname = usePathname();
   return (
-    <div>Bottombar</div>
+    <section className='bottombar'>
+      <div className='bottombar_container'>
+      {sidebarLinks.map((link) => {
+          const isActive = (pathname.includes(link.route) && link.route.length > 1 ) || (pathname === link.route);
+          return (
+            <Link href={link.route} key={link.label} className={`leftsidebar_link ${isActive && `bg-primary-500` }`} >
+              <Image src={link.imgURL} alt={link.label} width={24} height={24} />
+              <p className='text-subtle-medium text-light-1 max-sm:hidden'>{link.label.split(/\s+/)[0]}</p>
+            </Link>
+          )
+        })}
+    
+
+      </div>
+
+    </section>
   )
 }
 
--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -4,6 +4,7 @@ import { sidebarLinks } from "@/constants";
 import Link from 'next/link';
 import Image from 'next/image';
 import { usePathname , useRouter } from "next/navigation";
+import { SignedIn, SignOutButton } from '@clerk/nextjs';
 function LeftSidebar() {
   const router = useRouter();
   const pathname = usePathname();
@@ -20,7 +21,16 @@ function LeftSidebar() {
           )
         })}
       </div>
-
+        <div className='mt-10 px-6'>
+        <SignedIn>
+                        <SignOutButton signOutCallback={()=> router.push('/sign-in')}>
+                            <div className='flex cursor-pointer gap-4 p-4'>
+                                <Image src="/assets/logout.svg" alt="logoout" width={24} height={24}  />
+                                <p className='text-light-2 max-lg:hidden'>Logout</p>
+                            </div>
+                        </SignOutButton>
+                    </SignedIn>
+        </div>
     </section>
   )
 }
