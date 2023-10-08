db370211467021c9901a3abfe3e5a2c649ae8878
Sanjay Sirangi <sanjaysirangi@gmail.com>
2023-10-09 00:56:45 +0530

    🚀: created Froms and cards

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -15,7 +15,9 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
 
 import { useUploadThing } from "@/lib/uploadthing";
 import { isBase64Image } from "@/lib/utils";
