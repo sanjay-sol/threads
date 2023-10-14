0c08bb30d487cd4c259a4c1daf2cd8bdd2adee55
Sanjay Sirangi <sanjaysirangi@gmail.com>
2023-10-14 18:54:26 +0530

    🚀 : completed root && lib

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,44 +1,49 @@
-import '../globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import { ClerkProvider } from '@clerk/nextjs'
-import Topbar from '@/components/shared/Topbar'
-import LeftSidebar from '@/components/shared/LeftSidebar'
-import RightSidebar from '@/components/shared/RightSidebar'
-import Bottombar from '@/components/shared/Bottombar'
+import React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
-const inter = Inter({ subsets: ['latin'] })
+import "../globals.css";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import Bottombar from "@/components/shared/Bottombar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Topbar from "@/components/shared/Topbar";
 
-export const metadata: Metadata  = {
-  title: "Threads",
-  description: "Next js threads clone",
-}
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Insta-Threads",
+  description: "A Next.js 13 Meta Threads application",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
-      <html lang="en">
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
+      <html lang='en'>
         <body className={inter.className}>
           <Topbar />
+
           <main className='flex flex-row'>
             <LeftSidebar />
             <section className='main-container'>
-              <div className='w-full max-w-4xl'>
-                {children}
-              </div>
-
+              <div className='w-full max-w-4xl'>{children}</div>
             </section>
+            {/* @ts-ignore */}
             <RightSidebar />
           </main>
 
           <Bottombar />
         </body>
-
       </html>
     </ClerkProvider>
-  )
+  );
 }
--- app/(root)/profile/edit/page.tsx
+++ app/(root)/profile/edit/page.tsx
@@ -4,12 +4,14 @@ import { redirect } from "next/navigation";
 import { fetchUser } from "@/lib/actions/user.actions";
 import AccountProfile from "@/components/forms/AccountProfile";
 
+// Copy paste most of the code as it is from the /onboarding
+
 async function Page() {
   const user = await currentUser();
-  if (!user) return null; // to avoid typescript warnings
+  if (!user) return null;
 
   const userInfo = await fetchUser(user.id);
-  if (userInfo?.onboarded) redirect("/");
+  if (!userInfo?.onboarded) redirect("/onboarding");
 
   const userData = {
     id: user.id,
@@ -21,16 +23,14 @@ async function Page() {
   };
 
   return (
-    <main className='mx-auto flex max-w-3xl flex-col justify-start px-10 py-20'>
-      <h1 className='head-text'>Onboarding</h1>
-      <p className='mt-3 text-base-regular text-light-2'>
-        Complete your profile now, to use Threds.
-      </p>
+    <>
+      <h1 className='head-text'>Edit Profile</h1>
+      <p className='mt-3 text-base-regular text-light-2'>Make any changes</p>
 
-      <section className='mt-9 bg-dark-2 p-10'>
+      <section className='mt-12'>
         <AccountProfile user={userData} btnTitle='Continue' />
       </section>
-    </main>
+    </>
   );
 }
 
